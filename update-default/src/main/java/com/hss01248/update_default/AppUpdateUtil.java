25387f850ce1c7c9553d14d0103eebe8bb11a8a6
hss01248 <hss012489@gmail.com>
2022-11-25 19:15:57 +0800

    便捷api

---
+++ update-default/src/main/java/com/hss01248/update_default/AppUpdateUtil.java
@@ -0,0 +1,30 @@
+package com.hss01248.update_default;
+
+
+import com.vector.update_app.UpdateAppManager;
+import com.vector.update_app.listener.ExceptionHandler;
+
+/**
+ * @Despciption todo
+ * @Author hss
+ * @Date 28/07/2022 17:33
+ * @Version 1.0
+ */
+public class AppUpdateUtil {
+
+
+
+    public static void doUpdate(String url,ExceptionHandler handler){
+        new UpdateAppManager
+                .Builder()
+                //当前Activity
+                // .setActivity(this)
+                //更新地址
+                .setUpdateUrl(url)
+                 .handleException(handler)
+                //实现httpManager接口的对象
+                //.setHttpManager(new UpdateHttpImpl())
+                .build()
+                .update();
+    }
+}
